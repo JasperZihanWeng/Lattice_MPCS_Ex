bd3f66a132eba80579e2b244d258ee1c82f1c130
JasperZihanWeng <jasperweng@outlook.com>
2026-07-09 16:04:19 -0500

    changed internal clock path

--- rtl/Flashing_Light.v
+++ rtl/Flashing_Light.v
@@ -38,8 +38,19 @@ wire mpcs_lsync;
 wire mpcs_phyrdy;
 wire mpcs_ready;
 wire mpcs_rx_val;
+wire mpcs_resetn;
 
 assign fabric_clk = F_125Mhz_P;
+
+reg [15:0] reset_count = 16'd0;
+
+always @(posedge fabric_clk) begin
+    if (!mpcs_resetn) begin
+        reset_count <= reset_count + 1'b1;
+    end
+end
+
+assign mpcs_resetn = &reset_count;
 
 assign mpcs_tx_word = {78'h0, Switch1, Switch2};
 //rx_switch_value is the received signal for which light to turn on
@@ -59,9 +70,9 @@ end
 
 
 MPCS_ex u_mpcs (
-    .use_refmux_i(1'b0),
+    .use_refmux_i(1'b1),
     .diffioclksel_i(1'b0),
-    .clksel_i(2'b00),
+    .clksel_i(2'b11),
 
     .sdq_refclkp_q0_i(1'b0),
     .sdq_refclkn_q0_i(1'b0),
@@ -71,7 +82,7 @@ MPCS_ex u_mpcs (
     .sd_ext_1_refclk_i(1'b0),
     .pll_0_refclk_i(1'b0),
     .pll_1_refclk_i(1'b0),
-    .sd_pll_refclk_i(1'b0),
+    .sd_pll_refclk_i(fabric_clk),
 
     .acjtag_mode_i(1'b0),
     .acjtag_enable_i_0(1'b0),
@@ -82,7 +93,7 @@ MPCS_ex u_mpcs (
     .acjtagnout_o_0(),
 
     .lmmi_clk_i_0(fabric_clk),
-    .lmmi_resetn_i_0(1'b1),
+    .lmmi_resetn_i_0(mpcs_resetn),
     .lmmi_request_i_0(1'b0),
     .lmmi_wr_rdn_i_0(1'b0),
     .lmmi_offset_i_0(9'd0),
@@ -100,11 +111,11 @@ MPCS_ex u_mpcs (
 
     .mpcs_rx_usr_clk_i_0(rx_clk),
     .mpcs_tx_usr_clk_i_0(tx_clk),
-    .mpcs_tx_pcs_rstn_i_0(1'b1),
-    .mpcs_rx_pcs_rstn_i_0(1'b1),
+    .mpcs_tx_pcs_rstn_i_0(mpcs_resetn),
+    .mpcs_rx_pcs_rstn_i_0(mpcs_resetn && mpcs_phyrdy),
     .mpcs_rx_out_clk_o_0(rx_clk),
     .mpcs_tx_out_clk_o_0(tx_clk),
-    .mpcs_perstn_i_0(1'b1),
+    .mpcs_perstn_i_0(mpcs_resetn),
 
     .mpcs_tx_ch_din_i_0(mpcs_tx_word),
     .mpcs_tx_fifo_st_o_0(),
